c12af89e62107da9602a00af279996689d330dc6
Administrator <mydragon12345acgen@gmail.com>
2022-10-14 05:54:11 -0700

    added main file

---
+++ main.py
@@ -0,0 +1,20 @@
+from vkbottle import load_blueprints_from_package
+from vkbottle.bot import Bot
+
+from pytz import timezone
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from misc.server import Server
+
+
+if __name__ == '__main__':
+    server = Server('http://127.0.0.1:8910')
+    config = await server.send_message('config')
+    server.timezone = timezone(config.content.SERVER.timezone)
+    scheduler = AsyncIOScheduler()
+    scheduler.start()
+
+    bot = Bot(token=config.content.VK.token)
+    for blueprint in load_blueprints_from_package("blueprints"):
+        blueprint.load(bot)
+
+    bot.run_forever()
---
+++ misc/server.py
@@ -0,0 +1,26 @@
+import aiohttp
+from box import Box
+
+
+class Server:
+    def __init__(self, address: str):
+        self.session = aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=0))
+        self.address = address
+        self.messenger = 'VK'
+        self.timezone = None
+
+    async def send_message(self, endpoint: str, data: dict | list = None):
+        try:
+            use_method = self.session.get(f'{self.address}/{endpoint}') if not data \
+                else self.session.post(f'{self.address}/{endpoint}', json=data)
+
+            async with use_method as request:
+                response = await request.json()
+                response = Box(response)
+        except:
+            response = None
+
+        return response
+
+    async def close(self):
+        await self.session.close()
