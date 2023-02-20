3aa778adf49caed42094044fd4866b169c1101ff
Crow <mydragon12345acgen@gmail.com>
2023-02-20 20:57:21 +0100

    Добавлено скачивание и загрузка файлов

--- misc/connections/downloads.py
+++
@@ -1,9 +0,0 @@
-from aiohttp import ClientSession
-
-
-async def download_raw_file(url):
-    async with ClientSession() as session:
-        async with session.get(url) as response:
-            a = await response.read()
-
-    return a
